5af456b1aa063e3ce2bafe01fa8d9c10e6c0f2c5
PavanKumar Kavvuri <PavanproJack@github.com>
2020-08-12 08:37:48 +0100

    Adds Clusters of Ripeness

--- Ripeness Analysis/clustering.py
+++ Ripeness Analysis/clustering.py
@@ -2,17 +2,24 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import os
+import glob
 
-path = "/Users/pavankumar/Documents/Processed Datasets/scripts/Images/MaskedImages/"
+path = "/Users/pavankumar/Documents/Robotics MSc/Dissertation/Fruit-Detection-in-Orchards/Ripeness Analysis/msk_ripen"
 
 os.chdir(path)
 
-io = cv2.imread("mango_img_702_output.png")
+all_files = glob.glob("*")
+imgList = []
+for img in all_files:
+    imgList.append(img)
+
+print((imgList))
+
+original_image = cv2.imread(imgList[0])
 #original_image = cv2.imread("berry.jpg")
 #print(original_image)
 
-
-original_image = io[170:230, 200:245]
+#original_image = io[170:230, 200:245]
 
 img=cv2.cvtColor(original_image,cv2.COLOR_BGR2HSV)
 
@@ -22,7 +29,7 @@ vectorized = np.float32(vectorized)
 
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
 
-K = 3
+K = 2
 attempts=10
 ret,label,center=cv2.kmeans(vectorized,K,None,criteria,attempts,cv2.KMEANS_RANDOM_CENTERS)
 
@@ -39,15 +46,5 @@ plt.title('Original Image'), plt.xticks([]), plt.yticks([])
 plt.subplot(1,2,2),plt.imshow(result_image)
 plt.title('Segmented Image when K = %i' % K), plt.xticks([]), plt.yticks([])
 plt.show()
-
-
-
-'''edges = cv2.Canny(img,150,200)
-plt.figure(figsize=(figure_size,figure_size))
-plt.subplot(1,2,1),plt.imshow(img)
-plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(1,2,2),plt.imshow(edges,cmap = 'gray')
-plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
-plt.show()'''
 
 
